46befa658e39b69039d1c571d649e180a038d3ee
jeff-dillon <dillonjg@gmail.com>
2023-07-07 13:43:31 -0400

    refactor solution

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,12 +1,2 @@
 # add your code here
-for i in range(1, 101):
-    divisible_by_three = i % 3 == 0
-    divisible_by_five = i % 5 == 0
-    if(divisible_by_three & divisible_by_five):
-        print('FizzBuzz')
-    elif(divisible_by_three):
-        print('Fizz')
-    elif(divisible_by_five):
-        print('Buzz')
-    else:
-        print(i)
+
