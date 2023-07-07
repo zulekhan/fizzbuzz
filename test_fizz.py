f3413df4ac6e2136843948884f5ca5fd94cb92eb
jeff-dillon <dillonjg@gmail.com>
2023-07-07 13:42:24 -0400

    refactor test

--- test_fizz.py
+++ test_fizz.py
@@ -4,17 +4,17 @@ import io
 
 def test_fizz(monkeypatch):
     mocked_stdout = io.StringIO()
-    expected_output = "12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz1617Fizz19\
-        BuzzFizz2223FizzBuzz26Fizz2829FizzBuzz3132Fizz34BuzzFizz3738FizzBuzz\
-        41Fizz4344FizzBuzz4647Fizz49BuzzFizz5253FizzBuzz56Fizz5859FizzBuzz\
-        6162Fizz64BuzzFizz6768FizzBuzz71Fizz7374FizzBuzz7677Fizz79\
-        BuzzFizz8283FizzBuzz86Fizz8889FizzBuzz9192Fizz94BuzzFizz\
-        9798FizzBuzz"
+    expected_output = "".join("""
+        12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz1617Fizz19BuzzFizz2223
+        FizzBuzz26Fizz2829FizzBuzz3132Fizz34BuzzFizz3738FizzBuzz41Fizz4344
+        FizzBuzz4647Fizz49BuzzFizz5253FizzBuzz56Fizz5859FizzBuzz6162Fizz64
+        BuzzFizz6768FizzBuzz71Fizz7374FizzBuzz7677Fizz79BuzzFizz8283
+        FizzBuzz86Fizz8889FizzBuzz9192Fizz94BuzzFizz9798FizzBuzz""".split())
 
     with monkeypatch.context() as m:
         m.setattr(sys, "stdout", mocked_stdout)
         sys.modules.pop("fizzbuzz", None)
-        importlib.import_module(name="fizzbuzz", package="files")
-    
-    result = mocked_stdout.getvalue()
-    assert "".join(result.split()) == "".join(expected_output.split())
+        importlib.import_module(name="fizzbuzz", package="files")    
+        result = mocked_stdout.getvalue()
+        result = "".join(result.split()) # remove all whitespace characters
+        assert result == expected_output
