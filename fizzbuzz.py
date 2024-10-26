977956cc41c8345e2c85d368bddd3cbc269e0291
Zulekha Mohamed <zulekhamohamed@Zulekhas-MBP.lan>
2024-10-26 11:37:47 -0400

    Added FizzBuzz solution

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,17 @@
 # add your code here
 
+numbers = range(1, 101)
+
+for num in numbers:
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
+    
+
+
+
